a6764e3cfd383a2e288b4bf163800bcdf7a4fa6d
JorG96 <jecamg96@gmail.com>
2021-10-03 20:58:32 -0400

    views context updated

--- geoApp/views.py
+++ geoApp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 import folium
+from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    m=folium.Map(location=[-16.22,-71.59],zoom_start=10#)  
+    m=folium.Map(location=[4.703,-74.1],zoom_start=17)  
         ## exporting
     m=m._repr_html_()
     context= {'my_map': m}
